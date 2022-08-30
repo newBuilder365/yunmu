6373d390797453fa025a83ac4dc9c2159149b41d
newBuilder365 <772512900@qq.com>
2022-08-30 16:51:16 +0800

    引入组件样式库以及axios

--- src/components/usePinia.vue
+++ src/components/usePinia.vue
@@ -1,20 +1,16 @@
 <template>
-  <h1> 测试 vueUse 的鼠标坐标 </h1>
+  <h1>测试 vueUse 的鼠标坐标</h1>
   <h3>Mouse: {{ x }} x {{ y }}</h3>
+  <a-button type="primary">ant design</a-button>
+  <br />
+  <el-button type="primary">element plus</el-button>
 </template>
-  
-  <script lang="ts">
-  import { defineComponent } from 'vue';
-  import { useMouse } from '@vueuse/core'
-  
-  export default defineComponent({
-    name: 'VueUse',
-    setup() {
-      const { x, y } = useMouse()
-  
-      return {
-        x, y
-      }
-    }
-  });
-  </script>
+
+<script lang="ts" setup>
+import { useMouse } from "@vueuse/core";
+// import Api from '@/api';
+const { x, y } = useMouse();
+// const requestRes = async () => {
+//   let result = await Api.login('张三','123')
+// }
+</script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,53 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import * as path from 'path';
+import { UserConfigExport } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import * as path from 'path'
+import {
+  AntDesignVueResolver,
+  ElementPlusResolver
+} from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default (): UserConfigExport => {
+  return {
     resolve: {
-        //设置别名
-        alias: {
-            '@': path.resolve(__dirname, 'src')
-        }
+      //设置别名
+      alias: {
+        '@': path.resolve(__dirname, 'src')
+      }
     },
-    plugins: [vue()],
-    server: {
-        port: 8080, //启动端口
-        hmr: {
-            host: '127.0.0.1',
-            port: 8080
+    // 1. If you are using the ant-design series, you need to configure this
+    // 2. Make sure less is installed in the dependency `yarn add less -D`
+    css: {
+      preprocessorOptions: {
+        less: {
+          javascriptEnabled: true,
         },
-        // 设置 https 代理
-        // proxy: {
-        //     '/api': {
-        //         target: 'your https address',
-        //         changeOrigin: true,
-        //         rewrite: (path: string) => path.replace(/^\/api/, '')
-        //     }
-        // }
+      },
+    },
+    plugins: [
+      vue(),
+      Components({
+        dts: true,
+        resolvers: [
+          AntDesignVueResolver(),
+          ElementPlusResolver()
+        ],
+      })
+    ],
+    server: {
+      port: 8080, //启动端口
+      hmr: {
+        host: '127.0.0.1',
+        port: 8080
+      },
+      // 设置 https 代理
+      // proxy: {
+      //     '/api': {
+      //         target: 'your https address',
+      //         changeOrigin: true,
+      //         rewrite: (path: string) => path.replace(/^\/api/, '')
+      //     }
+      // }
     }
-});
+  }
+}
