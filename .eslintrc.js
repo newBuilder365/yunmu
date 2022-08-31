728d1a792a2c0fec28799c96295700d0efbac1d7
newBuilder365 <772512900@qq.com>
2022-08-31 14:26:25 +0800

    eslint生效

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,21 @@
 module.exports = {
-  parser: 'vue-eslint-parser',
-
+  parser: "vue-eslint-parser",
   parserOptions: {
-      parser: '@typescript-eslint/parser',
-      ecmaVersion: 2020,
-      sourceType: 'module',
-      ecmaFeatures: {
-          jsx: true
-      }
+    parser: "@typescript-eslint/parser",
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
-
   extends: [
-    'plugin:vue/vue3-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-    'plugin:prettier/recommended'
+    "plugin:vue/vue3-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "plugin:prettier/recommended",
   ],
 
   rules: {
-      // override/add rules settings here, such as:
-  }
+    // override/add rules settings here, such as:
+  },
 };
